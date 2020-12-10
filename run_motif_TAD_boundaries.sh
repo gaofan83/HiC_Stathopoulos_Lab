7075bba48dca425563a20c90974236020f0d072e
Fan Gao <gaofan83@hotmail.com>
2020-12-10 11:24:53 -0800

    Create run_motif_TAD_boundaries.sh

---
+++ run_motif_TAD_boundaries.sh
@@ -0,0 +1,4 @@
+findMotifsGenome.pl boundaries_2kb_c12_dm6.bed dm6 TAD_boundary_c12_MotifOutput_2kb/ -size 2000 -p 20 &
+findMotifsGenome.pl boundaries_2kb_c13_dm6.bed dm6 TAD_boundary_c13_MotifOutput_2kb/ -size 2000 -p 20 &
+findMotifsGenome.pl boundaries_2kb_c14_dm6.bed dm6 TAD_boundary_c14_MotifOutput_2kb/ -size 2000 -p 20 &
+findMotifsGenome.pl boundaries_2kb_3-4h_dm6.bed dm6 TAD_boundary_3-4h_MotifOutput_2kb/ -size 2000 -p 20 &
